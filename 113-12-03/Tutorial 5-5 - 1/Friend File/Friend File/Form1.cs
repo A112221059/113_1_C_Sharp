509f914a943368ebe243d653b3a44620364cc5f6
A112221059 <A112221059@mail.shu.edu.tw>
2024-12-03 18:02:48 +0800

    113-12-03

---
+++ 113-12-03/Tutorial 5-5 - 1/Friend File/Friend File/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq.Expressions;
+
+namespace Friend_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void writeNameButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter outputFile;
+                if (saveFile.showDialog() == DialogResult.OK)
+                {
+                    outputFile = File.AppendText(saveFile.FileName);
+                    outputFile.WriteLine(nameTextBox.Text);
+                    outputFile.Close();
+                    MassageBox.show("名子以寫入檔案");
+                    outputFile.Close();
+                }
+                else
+                {
+                    MessageBox.Show();
+                }
+            catch (Exception.ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+    private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單
+            this.Close();
+        }
+    }
+}
---
+++ 113-12-03/Tutorial 5-5/Friend File/Friend File/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Linq.Expressions;
+
+namespace Friend_File
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void writeNameButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                StreamWriter outputFile;
+
+                outputFile = File.AppendText(@"date\Friends.txt");
+                outputFile.WriteLine(nameTextBox.Text);
+                outputFile.Close();
+                MassageBox.show("名子" + "以寫入");
+            }
+            catch (Exception.ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    private void exitButton_Click(object sender, EventArgs e)
+        {
+            // 關閉表單
+            this.Close();
+        }
+    }
+}
